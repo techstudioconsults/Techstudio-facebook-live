fb51d3f0498b69c3a8697acf9f3ab1528a06947b
Tobi Olanitori <tobiolanitori@gmail.com>
2023-01-30 00:13:14 +0100

    build: preparing to deploy

---
+++ server.cjs
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(9000)
